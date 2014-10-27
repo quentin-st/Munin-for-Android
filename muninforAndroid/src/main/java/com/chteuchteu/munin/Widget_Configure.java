8dfbd8f21b9450cbfcc4d9744b94610cc5ca5fe0
Quentin S <chteuchteu@gmail.com>
2014-10-27 15:30:39 +0100

    Widget_Configure : light theme

--- muninforAndroid/src/main/java/com/chteuchteu/munin/Widget_Configure.java
+++ muninforAndroid/src/main/java/com/chteuchteu/munin/Widget_Configure.java
@@ -1,10 +1,13 @@
 package com.chteuchteu.munin;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -14,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chteuchteu.munin.obj.MuninPlugin;
 import com.chteuchteu.munin.obj.MuninServer;
@@ -53,33 +57,39 @@ public class Widget_Configure extends Activity {
 			final Widget widget = new Widget();
 			widget.setWidgetId(mAppWidgetId);
 			
-			setContentView(R.layout.widget_configuration);
-			
 			if (muninFoo != null) {
-				final ListView lv1 = (ListView) findViewById(R.id.listview1); // servers
-				final ListView lv2 = (ListView) findViewById(R.id.listview2); // plugins
-				final ListView lv3 = (ListView) findViewById(R.id.listview3); // period
-				final TextView tv = (TextView) findViewById(R.id.widgetConfiguration_textview);
+				LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				final View dialogView = mInflater.inflate(R.layout.widget_configuration, null);
+
+				final Context context = new ContextThemeWrapper(this, android.R.style.Theme_Holo_Light);
+
+				new AlertDialog.Builder(context)
+					.setView(dialogView)
+					.show();
+
+				final ListView lv1 = (ListView) dialogView.findViewById(R.id.listview1); // servers
+				final ListView lv2 = (ListView) dialogView.findViewById(R.id.listview2); // plugins
+				final ListView lv3 = (ListView) dialogView.findViewById(R.id.listview3); // period
 				
 				if (muninFoo.getServers().size() == 0) {
-					tv.setText(R.string.text37);
-					tv.setVisibility(View.VISIBLE);
+					Toast.makeText(this, R.string.text37, Toast.LENGTH_SHORT).show();
+					finish();
 				}
 				if (!muninFoo.premium) {
-					tv.setText(R.string.text58);
-					tv.setVisibility(View.VISIBLE);
+					Toast.makeText(this, "Munin for Android features pack needed", Toast.LENGTH_SHORT).show();
+					finish();
 				}
 				
 				ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
 				list.clear();
 				HashMap<String,String> item;
-				for(int i=0; i<muninFoo.getOrderedServers().size(); i++){
+				for(MuninServer server : muninFoo.getOrderedServers()) {
 					item = new HashMap<String,String>();
-					item.put("line1", muninFoo.getOrderedServers().get(i).getName());
-					item.put("line2", muninFoo.getOrderedServers().get(i).getServerUrl());
+					item.put("line1", server.getName());
+					item.put("line2", server.getServerUrl());
 					list.add(item);
 				}
-				SimpleAdapter sa = new SimpleAdapter(this, list, R.layout.servers_list_dark, new String[] { "line1","line2" }, new int[] {R.id.line_a, R.id.line_b});
+				SimpleAdapter sa = new SimpleAdapter(context, list, R.layout.servers_list, new String[] { "line1","line2" }, new int[] {R.id.line_a, R.id.line_b});
 				lv1.setAdapter(sa);
 				lv1.setOnItemClickListener(new OnItemClickListener() {
 					public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
@@ -97,19 +107,20 @@ public class Widget_Configure extends Activity {
 						ArrayList<HashMap<String,String>> list2 = new ArrayList<HashMap<String,String>>();
 						list2.clear();
 						HashMap<String,String> item2;
-						for(int i=0; i<plugins.size(); i++){
+						for (MuninPlugin plugin : plugins) {
 							item2 = new HashMap<String,String>();
-							item2.put("line1", plugins.get(i).getFancyName());
-							item2.put("line2", plugins.get(i).getName());
+							item2.put("line1", plugin.getFancyName());
+							item2.put("line2", plugin.getName());
 							list2.add(item2);
 						}
-						SimpleAdapter sa2 = new SimpleAdapter(Widget_Configure.this, list2, R.layout.plugins_list_dark, new String[] { "line1","line2" }, new int[] {R.id.line_a, R.id.line_b});
+						SimpleAdapter sa2 = new SimpleAdapter(context, list2, R.layout.plugins_list,
+								new String[] { "line1","line2" }, new int[] {R.id.line_a, R.id.line_b});
 						lv2.setAdapter(sa2);
 						
 						lv2.setOnItemClickListener(new OnItemClickListener() {
 							public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
 								String pluginName = ((TextView) view.findViewById(R.id.line_b)).getText().toString();
-								
+
 								for (MuninPlugin p : selectedServer.getPlugins()) {
 									if (p.getName().equals(pluginName))
 										widget.setPlugin(p);
@@ -118,28 +129,29 @@ public class Widget_Configure extends Activity {
 								lv2.setVisibility(View.GONE);
 								
 								ArrayList<HashMap<String,String>> list3 = new ArrayList<HashMap<String,String>>();
-								list3.clear();
-								HashMap<String,String> item3;
-								String[] periods = {"day", "week", "month", "year" };
-								for (int i=0; i<4; i++) {
-									item3 = new HashMap<String,String>();
-									item3.put("line1", periods[i]);
+
+								String[] periods = { getString(R.string.text47_1), getString(R.string.text47_2),
+										getString(R.string.text47_3), getString(R.string.text47_4) };
+								for (String str : periods) {
+									HashMap<String,String> item3 = new HashMap<String,String>();
+									item3.put("line1", str);
 									list3.add(item3);
 								}
-								SimpleAdapter sa3 = new SimpleAdapter(Widget_Configure.this, list3, R.layout.widget_periodselection_dark, new String[] { "line1" }, new int[] {R.id.line_a});
+								SimpleAdapter sa3 = new SimpleAdapter(context, list3, R.layout.widget_periodselection,
+										new String[] { "line1" }, new int[] {R.id.line_a});
 								lv3.setAdapter(sa3);
 								lv3.setOnItemClickListener(new OnItemClickListener() {
 									public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
 										String[] periods = {"day", "week", "month", "year" };
 										widget.setPeriod(periods[position]);
 										
-										LinearLayout ll = (LinearLayout) findViewById(R.id.final_instructions);
+										LinearLayout ll = (LinearLayout) dialogView.findViewById(R.id.final_instructions);
 										lv3.setVisibility(View.GONE);
 										ll.setVisibility(View.VISIBLE);
 										ll.requestFocus();
-										final CheckBox cb = (CheckBox) findViewById(R.id.checkbox_wifi);
-										final CheckBox cb2 = (CheckBox) findViewById(R.id.checkbox_hidetitle);
-										Button btn = (Button) findViewById(R.id.save);
+										final CheckBox cb = (CheckBox) dialogView.findViewById(R.id.checkbox_wifi);
+										final CheckBox cb2 = (CheckBox) dialogView.findViewById(R.id.checkbox_hidetitle);
+										Button btn = (Button) dialogView.findViewById(R.id.save);
 										
 										btn.setOnClickListener(new View.OnClickListener() {
 											public void onClick(View v) {
@@ -148,7 +160,7 @@ public class Widget_Configure extends Activity {
 												widget.setHideServerName(cb2.isChecked());
 												
 												muninFoo.sqlite.dbHlpr.insertWidget(widget);
-												//muninFoo.sqlite.logWidgets();
+
 												configureWidget(getApplicationContext());
 												
 												// Make sure we pass back the original appWidgetId before closing the activity
