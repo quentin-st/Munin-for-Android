f4a5eaa862e1c91714b50a2e1121ae304ad84494
Quentin S <chteuchteu@gmail.com>
2015-07-12 17:42:52 +0200

    Ready for 3.6.3-beta1 update

--- muninforAndroid/src/main/java/com/chteuchteu/munin/MuninFoo.java
+++ muninforAndroid/src/main/java/com/chteuchteu/munin/MuninFoo.java
@@ -37,7 +37,7 @@ public class MuninFoo {
 
 	private String userAgent;
 
-	public static final double VERSION = 6.2;
+	public static final double VERSION = 6.3;
 	private static final boolean FORCE_NOT_PREMIUM = false;
 
     // Allows an user to test the app until the TRIAL_EXPIRATION date is reached
