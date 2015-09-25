58a548fd89419f184c05836de6fe869fbc398a4f
Quentin S <chteuchteu@gmail.com>
2015-09-25 16:12:31 +0200

    Remove old migration code

--- muninforAndroid/src/main/java/com/chteuchteu/munin/hlpr/SQLite.java
+++ muninforAndroid/src/main/java/com/chteuchteu/munin/hlpr/SQLite.java
@@ -10,9 +10,6 @@ import com.chteuchteu.munin.obj.Label;
 import com.chteuchteu.munin.obj.MuninMaster;
 import com.chteuchteu.munin.obj.MuninNode;
 import com.chteuchteu.munin.obj.MuninPlugin;
-import com.chteuchteu.munin.obj.MuninMaster.AuthType;
-
-import java.util.ArrayList;
 
 public class SQLite {
 	private MuninFoo muninFoo;
@@ -43,126 +40,6 @@ public class SQLite {
 			
 			for (MuninPlugin p : l.plugins)
 				dbHlpr.insertLabelRelation(p, l);
-		}
-	}
-	
-	public void migrateTo3() {
-		// Here, columns have already been created.
-		// Let's check if there are some MuninMaster (_not default_)
-		// whose attributes needs to be filled
-		
-		for (MuninMaster master : muninFoo.getMasters()) {
-			// DefaultMaster groups several heterogeneous nodes.
-			// We can't easily group credentials here
-			if (!master.defaultMaster && !master.isEmpty()) {
-				// Get the first node
-				MuninNode model = master.getChildren().get(0);
-				
-				getOldAuthInformation(model.getId(), master, true);
-			}
-		}
-		
-		// Merge defaultMaster childrens by host name. If necessary get old auth information.
-		// Find default master
-		MuninMaster defaultMaster = null;
-		for (MuninMaster master : muninFoo.getMasters()) {
-			if (master.defaultMaster) {
-				defaultMaster = master;
-				break;
-			}
-		}
-		
-		// We don't necessarily have a default master
-		if (defaultMaster != null) {
-			ArrayList<MuninMaster> newMasters = new ArrayList<>();
-			
-			for (MuninNode node : defaultMaster.getChildren()) {
-				// Check if there already is a master for this node in newMasters
-				String masterUrl = Util.URLManipulation.ascendDirectory(2, node.getUrl());
-				
-				MuninMaster parent = null;
-				boolean contains = false;
-				for (MuninMaster master : newMasters) {
-					if (master.getUrl().equals(masterUrl)) {
-						contains = true;
-						parent = master;
-						break;
-					}
-				}
-				
-				// Doesn't contains => add
-				if (!contains) {
-					String masterName = Util.URLManipulation.getHostFromUrl(node.getUrl());
-					
-					parent = new MuninMaster();
-					parent.setName(masterName);
-					parent.setUrl(masterUrl);
-					newMasters.add(parent);
-				}
-				
-				// Attach node to parent
-				parent.addChild(node);
-				
-				if (!contains) {
-					// Get auth ids if needed. Only needed to be done the first time
-					// (since all the nodes should have the same auth ids)
-					getOldAuthInformation(node.getId(), parent, false);
-				}
-			}
-			
-			// Insert masters and update children
-			for (MuninMaster master : newMasters) {
-				dbHlpr.insertMuninMaster(master);
-				for (MuninNode node : master.getChildren())
-					dbHlpr.updateMuninNode(node);
-			}
-			
-			// Delete default master
-			dbHlpr.deleteMaster(defaultMaster, false);
-		}
-	}
-	
-	/**
-	 * Before Munin for Android 3.0, auth ids were attached to MuninNode.
-	 * Those auth information weren't deleting during update so they can
-	 * be fetched back for migration purposes.
-	 * @param muninNodeId Information source (MuninNode id)
-	 * @param attachTo Information destination
-	 * @param saveChanges boolean
-	 */
-	private void getOldAuthInformation(long muninNodeId, MuninMaster attachTo, boolean saveChanges) {
-		String KEY_MUNINNODES_AUTHLOGIN = "authLogin";
-		String KEY_MUNINNODES_AUTHPASSWORD = "authPassword";
-		String KEY_MUNINNODES_SSL = "SSL";
-		String KEY_MUNINNODES_AUTHTYPE = "authType";
-		String KEY_MUNINNODES_AUTHSTRING = "authString";
-		
-		String selectQuery = "SELECT * FROM " + DatabaseHelper.TABLE_MUNINNODES
-				+ " WHERE " + DatabaseHelper.KEY_ID + " = " + muninNodeId;
-		
-		SQLiteDatabase db = dbHlpr.getReadableDatabase();
-		Cursor c = db.rawQuery(selectQuery, null);
-		
-		if (c.moveToFirst()) { // The node has been found (which should always be the case)
-			AuthType authType = AuthType.get(c.getInt(c.getColumnIndex(KEY_MUNINNODES_AUTHTYPE)));
-			
-			attachTo.setAuthType(authType);
-			if (authType == AuthType.BASIC || authType == AuthType.DIGEST) {
-				String authLogin = c.getString(c.getColumnIndex(KEY_MUNINNODES_AUTHLOGIN));
-				String authPassword = c.getString(c.getColumnIndex(KEY_MUNINNODES_AUTHPASSWORD));
-				String authString = c.getString(c.getColumnIndex(KEY_MUNINNODES_AUTHSTRING));
-				
-				attachTo.setAuthIds(authLogin, authPassword);
-				attachTo.setAuthString(authString);
-			}
-			
-			boolean ssl = c.getInt(c.getColumnIndex(KEY_MUNINNODES_SSL)) == 1;
-			attachTo.setSSL(ssl);
-			
-			DatabaseHelper.close(c, db);
-			
-			if (saveChanges)
-				dbHlpr.saveMuninMaster(attachTo);
 		}
 	}
 
