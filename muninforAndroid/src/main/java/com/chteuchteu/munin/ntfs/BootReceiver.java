220197f4b70b2662dd2543618d1398a01aadc4d7
Quentin Stoeckel <chteuchteu@gmail.com>
2015-09-27 11:41:52 +0200

    Fix BootReceiver

--- muninforAndroid/src/main/java/com/chteuchteu/munin/ntfs/BootReceiver.java
+++ muninforAndroid/src/main/java/com/chteuchteu/munin/ntfs/BootReceiver.java
@@ -23,21 +23,17 @@ public class BootReceiver extends BroadcastReceiver {
 
 		// In our case intent will always be BOOT_COMPLETED, so we can just set the alarm
 		if (settings.getBool(Settings.PrefKeys.Notifications)) {
-			int min = 0;
-
-			String refreshRate = settings.getString(Settings.PrefKeys.Notifs_RefreshRate);
-			if (!refreshRate.equals(""))
-				min = Integer.parseInt(refreshRate);
+			int refreshRate = settings.getInt(Settings.PrefKeys.Notifs_RefreshRate, -1);
 			
 			AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 			Intent i = new Intent(context, Service_Notifications.class);
 			PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
 			am.cancel(pi);
 			
-			if (min > 0) {
+			if (refreshRate > 0) {
 				am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-						SystemClock.elapsedRealtime() + min*60*1000,
-						min*60*1000, pi);
+						SystemClock.elapsedRealtime() + refreshRate*60*1000,
+						refreshRate*60*1000, pi);
 			}
 		}
 	}
